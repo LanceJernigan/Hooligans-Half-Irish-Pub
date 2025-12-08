86dc637be1c591e2aae17031a0384902527272d6
LanceJernigan <lance@lancejernigan.com>
2025-12-08 17:43:25 -0500

    Add about component

---
+++ components/about/index.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
+const About = () => {
+	return (
+		<section className={styles.component}>
+			<div className={styles.wrapper}>
+				<h2>
+					A Local Favorite
+					<br />
+					Since 2012
+				</h2>
+				<p>
+					Hooligans Half Irish Pub has been Cookeville’s gathering place for
+					good food and good company for more than a decade. The bar is locally
+					owned and run by people who love their community. Whether you are here
+					for a pint, a game, or a meal with friends, you will find friendly
+					faces and a laid back atmosphere every time you visit.
+				</p>
+			</div>
+			<div className={styles.background}>
+				<Image
+					src="/assets/images/bottles.jpg"
+					alt="chalkboard texture"
+					fill
+					className="object-cover object-center z-0"
+					sizes="100vw"
+					quality={100}
+				/>
+			</div>
+		</section>
+	);
+};
+
+export default About;
