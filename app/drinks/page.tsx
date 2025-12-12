7c6dc9f02e40ccc25af89ebaeb60ce95eac81cbc
LanceJernigan <lance@lancejernigan.com>
2025-12-11 20:15:47 -0500

    Fix api call

--- app/drinks/page.tsx
+++ app/drinks/page.tsx
@@ -1,10 +1,21 @@
 import Drinks from "@/components/drinks";
 
 const Page = async () => {
-	const drinksResponse = await fetch(`${process.env.VERCEL_URL}/api/menus`, {
-		cache: "no-store",
-	});
+	const drinksResponse = await fetch(
+		`${process.env.NODE_ENV === "development" ? "http" : "https"}://${
+			process.env.VERCEL_URL
+		}/api/menus`,
+		{
+			cache: "no-store",
+		},
+	);
 	const menus = await drinksResponse.json();
+
+	console.log(
+		`${process.env.NODE_ENV === "development" ? "http" : "https"}://${
+			process.env.VERCEL_URL
+		}/api/menus`,
+	);
 
 	return <Drinks menus={menus} />;
 };
